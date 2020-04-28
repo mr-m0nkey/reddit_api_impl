cb15153f250078dd12ee78a1175e1345c19624ea
Mayowa Aj <sagaraaj69@gmail.com>
2020-04-28 11:36:23 +0100

    model "constructors"

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -3,8 +3,10 @@
 #[derive(Debug)]
 pub struct Post {
     title: String,
-    body: String,
-    is_edited: bool
+    body: Option<String>,
+    is_edited: bool,
+    upvotes: i32,
+    downvotes: i32
 }
 
 
@@ -12,7 +14,9 @@ pub struct Post {
 pub struct Comment {
     text: String,
     is_edited: bool,
-    replies: Vec<Comment>
+    replies: Vec<Comment>,
+    upvotes: i32,
+    downvotes: i32
 }
 
 #[derive(Debug)]
@@ -21,3 +25,42 @@ pub struct User {
     password: String
 }
 
+// implementations
+impl Post {
+
+    pub fn new(title: &str, body: Option<String>) -> Post {
+        Post {
+            title: title.to_string(),
+            body: body,
+            is_edited: false,
+            upvotes: 0,
+            downvotes: 0
+        }
+    }
+
+}
+
+impl Comment {
+
+    pub fn new(text: &str) -> Comment {
+        Comment {
+            text: text.to_string(),
+            is_edited: false,
+            replies: Vec::new(),
+            upvotes: 0,
+            downvotes: 0
+        }
+    }
+
+}
+
+impl User {
+    
+    pub fn new(username: &str, password: &str) -> User {
+        User {
+            username: username.to_string(),
+            password: password.to_string()
+        }
+    }
+
+}
