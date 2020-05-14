c66fc2fc95b6067b4a0f4621a8a7d231cf033766
Mayowa Aj <sagaraaj69@gmail.com>
2020-05-14 23:17:05 +0100

    added new user bindings

--- src/data/bindings.rs
+++ src/data/bindings.rs
@@ -1,3 +1,5 @@
+use super::models::*;
+use crate::rocket::request::Form;
 use crate::schema::users;
 
 #[derive(Insertable)]
@@ -5,4 +7,38 @@ use crate::schema::users;
 pub struct NewUser {
     pub username: String,
     pub password: String,
+}
+
+impl NewUser {
+    fn from_user(user: User) -> NewUser {
+        NewUser {
+            username: user.username,
+            password: user.password,
+        }
+    }
+}
+
+#[derive(FromForm)]
+pub struct UserForm {
+    pub username: String,
+    pub password: Option<String>,
+}
+
+impl UserForm {
+    pub fn update_user(self, user: &mut User) {
+        user.username = self.username;
+    }
+
+    pub fn to_new_user(self) -> NewUser {
+        //TODO return Result<NewUser> instead
+
+        if let Some(passwword) = self.password {
+            NewUser {
+                username: self.username,
+                password: passwword,
+            }
+        } else {
+            panic!("A password is required");
+        }
+    }
 }
